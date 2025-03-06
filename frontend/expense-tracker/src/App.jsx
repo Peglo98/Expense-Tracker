961a5f77c90112c8b6b7886fc23184f7ebb4e5aa
Patryk Główczyński <s92213@student.tu.kielce.pl>
2025-03-06 22:07:42 +0100

    first commit

---
+++ frontend/expense-tracker/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className="text-3xl text-purple-700">App</div>
+  )
+}
+
+export default App
